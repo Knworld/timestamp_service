4757ff84c2ee3a737f6243e5e1468c219c7a997f
Khuyen Nguyen <khuyenvnguyen@gmail.com>
2016-09-13 22:53:03 +0000

    complete

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
+var path = require('path');
+
+function parseTime(query){
+    var timeObj = {"unix":null, "natural":null};
+    var ptn_unix = /^\d+$/;
+    var thisMoment = moment(query);
+    
+    if(ptn_unix.test(query)){
+        thisMoment = moment(query, "X");
+        timeObj.unix = Number(query);
+        timeObj.natural = thisMoment.utc().format("MMMM DD, YYYY");
+    }else if(thisMoment.isValid()){
+        timeObj.unix = Number(thisMoment.utc().format("X"));
+        timeObj.natural = thisMoment.utc().format("MMMM DD, YYYY");
+    
+    }
+    
+    return JSON.stringify(timeObj);
+}
 
 app.get('/', function(req, res){
-   res.send('Hello World!') ;
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get('/:datestr', function(req, res){
+    var query = req.params.datestr;
+    res.end(parseTime(query))
+});
+
+/*
+app.use(function(req, res){
+   var query = req.path;
+   res.end(parseTime(decodeURI(query.slice(1))));
+});
+*/
+
 app.listen(8080, function(){
-    console.log('Example app listening on port 8080');
+    //console.log('Example app listening on port 8080');
 })
